ae8013aee57540ce08975415e0898c22a3b6bcd5
Thu Nguyen <34468120+thsunkid@users.noreply.github.com>
2022-09-15 23:39:42 +0700

    Update URL params

--- You Search Extension/SafariExtensionHandler.swift
+++ You Search Extension/SafariExtensionHandler.swift
@@ -18,7 +18,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     // E.g. Google on URL pattern on safari:  https://www.google.com/search?client=safari&rls=en&q=hello&ie=UTF-8&oe=UTF-8
     let safariSearchEngines: [SearchEngine] = [
-        SearchEngine(name: "Google", prefixUrl: "www.google.com/search?", specialCode: "client=safari&rls=en", queryParam: "q"),
+        SearchEngine(name: "Google", prefixUrl: "www.google.com/search?", specialCode: "client=safari", queryParam: "q"),
         SearchEngine(name: "Bing", prefixUrl: "www.bing.com/search?", specialCode: "form=APMCS1&PC=APMC", queryParam: "q"),
         SearchEngine(name: "Yahoo", prefixUrl: "search.yahoo.com/search?", specialCode: "fr=aaplw", queryParam: "p"),
         SearchEngine(name: "DuckDuckGo", prefixUrl: "duckduckgo.com/", specialCode: "t=osx", queryParam: "q"),
@@ -33,7 +33,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         if let searchEngine = safariSearchEngines.first(where: { urlString.contains($0.prefixUrl) }),
            urlString.contains(searchEngine.specialCode), // is the search made from the address address
            let userQuery = URLComponents(string: urlString)?.percentEncodedQueryItems?.first(where: { $0.name ==  searchEngine.queryParam })?.value {
-              return URL(string: youSearchURLPattern + userQuery)!
+              return URL(string: "https://you.com/search?q=\(userQuery)&fromExtension=true")!
             }
         return nil
     }
